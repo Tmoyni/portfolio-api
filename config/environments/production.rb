d24fa952e0a816fa3870c6c1c2238fb5ebd4ed75
github username <github email address>
2020-06-28 13:52:47 -0400

    updated action mailer url options to point to herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'new_app_name.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'https://moynihan-portfolio-api.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
